3908923d4b59e5696a8d6f190e90aad430cbeae6
Ahmed Mouzoune <ahmedmouzoune@MacBook-Pro-de-Ahmed.local>
2022-01-27 17:21:21 +0100

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
-    <>
-    <h1>CMS news</h1>
-    </>
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+    </div>
   );
 }
 
