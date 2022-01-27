5570df4591d2437f88aee0e5bd72cfbc74d07420
AHMOUZOUNE <ahmed_mouzoune@carrefour.com>
2022-01-27 18:14:27 +0100

    Add nested

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import {
   Route
 } from "react-router-dom";
 import App from "./App";
+import Navbar from "./Navbar";
 import Expenses from "./routes/expenses";
+import Invoice from "./routes/invoice";
 import Invoices from "./routes/invoices";
 
 const rootElement = document.getElementById("root");
@@ -14,7 +16,17 @@ render(
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+            <Route
+              index
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>Select an invoice</p>
+                </main>
+              }
+            />
+            <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
         <Route
           path="*"
           element={
